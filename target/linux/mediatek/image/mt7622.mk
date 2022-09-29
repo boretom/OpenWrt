bcfb4f79b3d2d2a90d7152f848ba8666ab175bb6
Marcel Ziswiler <marcel@ziswiler.com>
2022-09-29 22:40:31 +0200

    mediatek: add support for netgear wax206
    SoC: MediaTek MT7622BV
    RAM: DDR3 512 MiB (Nanya NT5CC256M16ER-EK)
    Flash: SPI-NAND 256 MiB (Toshiba TC58CVG1S3HRAIJ)
    WLAN: 2.4/5 GHz 4T4R
    2.4 GHz: MediaTek MT7622BV
    5 GHz: MediaTek MT7915AN/MT7975AN
    Ethernet: 4x 10/100/1000 Mbps LAN, 1x 10/100/1000/2500 Mbps WAN (Realtek RTL8221B PHY)
    Switch: MediaTek MT7531AE
    LEDs/Keys: 8/1 (Power, Internet, LAN1, LAN2, LAN3, LAN4, Wifin and Wifia dual-colour LEDs + Reset pin)
    UART: Marked J19 on board VCC GND TX RX, beginning from "1". 3.3v, 115200n8
    Power: 12 VDC, 2.5 A
    
    U-Boot allows booting an initramfs image via TFTP as follows:
    
    setenv ipaddr 192.168.10.2
    setenv serverip 192.168.10.1
    tftpboot openwrt-mediatek-mt7622-netgear_wax206-initramfs-recovery.itb
    bootm
    
    Signed-off-by: Marcel Ziswiler <marcel@ziswiler.com>

--- target/linux/mediatek/image/mt7622.mk
+++ target/linux/mediatek/image/mt7622.mk
@@ -228,6 +228,27 @@ define Device/mediatek_mt7622-rfb1-ubi
 endef
 TARGET_DEVICES += mediatek_mt7622-rfb1-ubi
 
+define Device/netgear_wax206
+  DEVICE_VENDOR := Netgear
+  DEVICE_MODEL := WAX206
+  DEVICE_DTS := mt7622-netgear-wax206
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7915e
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  UBOOTENV_IN_UBI := 1
+  KERNEL_IN_UBI := 1
+  KERNEL := kernel-bin | gzip
+# recovery can also be used with stock firmware web-ui, hence the padding...
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 128k
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  IMAGES := sysupgrade.itb
+  IMAGE/sysupgrade.itb := append-kernel | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
+endef
+TARGET_DEVICES += netgear_wax206
+
 define Device/ruijie_rg-ew3200gx-pro
   DEVICE_VENDOR := Ruijie
   DEVICE_MODEL := RG-EW3200GX PRO
